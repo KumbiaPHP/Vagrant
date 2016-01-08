305f506ee4379313a61ff48a3f769c4ceb233bc7
joanhey <joanhey@kumbiaphp.com>
2016-01-08 14:16:43 +0100

    php5.sh update

--- scripts/php5.sh
+++ scripts/php5.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+# For standalone use
+# php5.sh MODULE1 MODULE2 ...
+# TODO mejorar este hack
+if [ ! $1 ]
+then 
+    source /vagrant/config.cfg
+fi
+
 # Uninstall php7 for safety
 sudo apt-get remove php7.0  ^php7.0 -y -q
 
@@ -9,3 +17,4 @@ echo "======================================================="
 sudo apt-get install php5 $PHP5_MODS -y
 
 sudo php5enmod $PHP5_MODS
+                 
