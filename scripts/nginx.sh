3b942289838dc35ca68dc8d53e0861c66f6fd65c
joanhey <joanhey@kumbiaphp.com>
2016-01-08 12:43:38 +0100

    Added Nginx

--- scripts/nginx.sh
+++ scripts/nginx.sh
@@ -1,1 +1,10 @@
 #!/bin/bash
+
+# Stop apache2
+sudo service apache2 stop
+
+# Nginx
+echo "Installing Nginx"
+echo "========================================================================"
+apt-get install nginx -y
+
